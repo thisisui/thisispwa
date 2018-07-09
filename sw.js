4e52d3561dd311cdc4eae8d6cd8cee4a063de340
Igor Napierala <igor.napierala@cognifide.com>
2018-07-09 23:13:20 +0200

    sw updated

--- sw.js
+++ sw.js
@@ -12,3 +12,15 @@ self.addEventListener('install', function(e) {
         })
     );
 });
+
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request, { ignoreSearch: true }).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
